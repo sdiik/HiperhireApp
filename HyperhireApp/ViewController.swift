d70a3f085e4c5e0603e7f3290bcb4a64d17f8f94
sdiik <ahmadshiddiq0@gmail.com>
2024-12-06 20:11:37 +0700

    Initial Commit

---
+++ HyperhireApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HyperhireApp
+//
+//  Created by ahmad shiddiq on 06/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
